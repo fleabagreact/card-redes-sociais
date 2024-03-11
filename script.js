0b974e4e2a56f821cc1545fce1f88b99dbc94628
Luiza <99078781+fleabagreact@users.noreply.github.com>
2024-03-11 11:21:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function redirectToGitHub() {
+    window.open("https://github.com/fleabagreact", "_blank");
+}
+
+function redirectToTwitter() {
+    window.open("https://twitter.com/sylviephillia", "_blank");
+}
+
+function redirectToLetterboxd() {
+    window.open("https://boxd.it/5jKwh", "_blank");
+}
+
+function redirectToSpotify() {
+    window.open("https://open.spotify.com/user/96gjoojhzx0fb2bt5gqesqguy", "_blank");
+}
