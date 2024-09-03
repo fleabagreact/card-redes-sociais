3f2dbdfdd909b2f90063945115eac01f33c53b1c
Luiza. <luizadomato16@gmail.com>
2024-09-03 07:46:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ function redirectToGitHub() {
 }
 
 function redirectToLinkedIn() {
-    window.open("https://www.linkedin.com/lududo", "_blank");
+    window.open("https://www.linkedin.com/in/lududo/", "_blank");
 }
 
 function redirectToFigma() {
-    window.open("https://www.figma.com/luizasouzae", "_blank");
+    window.open("https://www.figma.com/files/team/1411422037020726407/user/1411420557855841907?fuid=1411420557855841907", "_blank");
 }
 
 function redirectToSpotify() {
